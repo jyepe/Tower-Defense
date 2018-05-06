250d93ff310470361011fd91e8200c86193176f2
jyepe <yepejy@gmail.com>
2018-05-06 15:15:08 -0400

    Added label on cube to know where in the world it is currently positioned

--- Assets/SnapInEdit.cs
+++ Assets/SnapInEdit.cs
@@ -3,45 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
+[SelectionBase]
 public class SnapInEdit : MonoBehaviour
 {
 
-    [SerializeField] [Range(1f,20f)] float gridSize;  //Size of playing field
+    [SerializeField] [Range(1f,20f)] float gridSize;  //Amount to move he cube by
+    TextMesh cubeText;
 
-	// Update is called once per frame
-	void Update ()
+    // Update is called once per frame
+    void Update ()
     {
         snapCube();
-        checkBoundaries();
     }
 
-    //Moves cube every 10 units in x and z axis
+    //Moves cube every 'gridSize' units in x and z axis
     private void snapCube()
     {
         Vector3 cubePos;
         cubePos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         cubePos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
         transform.position = new Vector3(cubePos.x, 0f, cubePos.z);
+        changeLabel(cubePos.x, cubePos.z);
     }
 
-    //Does not allow cubes to go out of gridsize
-    private void checkBoundaries()
+    private void changeLabel(float xPos, float zPos)
     {
-        if (transform.position.x > gridSize)
-        {
-            transform.position = new Vector3(gridSize, 0f, transform.position.z);
-        }
-        else if (transform.position.x < -gridSize)
-        {
-            transform.position = new Vector3(-gridSize, 0f, transform.position.z);
-        }
-        if (transform.position.z > gridSize)
-        {
-            transform.position = new Vector3(transform.position.x, 0f, gridSize);
-        }
-        else if (transform.position.z < -gridSize)
-        {
-            transform.position = new Vector3(transform.position.x, 0f, -gridSize);
-        }
+        cubeText = GetComponentInChildren<TextMesh>();
+        cubeText.text = xPos / gridSize + "," + zPos / gridSize;
     }
 }
