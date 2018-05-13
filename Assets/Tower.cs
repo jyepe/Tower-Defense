f749273a8247ba040a4bf5cd8fbf1bd7c44daad1
jyepe <yepejy@gmail.com>
2018-05-13 00:02:59 -0400

    Turrets look at enemy

---
+++ Assets/Tower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform towerTop;    //Top of turret
+    [SerializeField] Transform enemy;       //Enemy to look at
+    
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        towerTop.LookAt(enemy);
+	}
+}
